1fa54171336999b4de4a724d0a0e627cfdbf49f1
Bikram Das <bikramdas736136@gmail.com>
2024-03-19 19:44:28 +0530

    typing error corrected

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -18,7 +18,7 @@ class AuthenticatedSessionController extends Controller
     public function create(): View
     {
         return view('auth.login');
-    }w
+    }
 
     /**
      * Handle an incoming authentication request.
