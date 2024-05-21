2bd1f20355db80f9dc5586198676d7df7a8ec12f
Kunsang Moktan <kunsangmoktan11@gmail.com>
2024-05-21 20:34:22 +0530

    fixed admin login problem

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             'name' => 'Admin',
             'email' => 'admin@gmail.com',
             'password' => Hash::make(12345678),
+            'role' => 1,
         ]);
     }
 }
