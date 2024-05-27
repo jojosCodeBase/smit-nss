a6343a3ec3129856defa9a02081671028efcd310
Bikram Das <bikramdas736136@gmail.com>
2024-05-27 12:24:37 +0530

    Add validation on  add drive form

--- resources/views/admin/drives/add.blade.php
+++ resources/views/admin/drives/add.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts/admin-content')
 @section('breadcrumb', 'Add Drives')
 @section('content')
-@include('include/alerts')
+    @include('include/alerts')
     <div class="container-fluid p-0">
         <div class="card">
             <div class="card-body">
@@ -9,40 +9,51 @@
                 <span class="fw-bold">Drive id: {{ $id }}</span>
                 <form action="{{ route('drive.add') }}" method="POST">
                     @csrf
+                    {{-- <div class="input-group has-validation"> --}}
                     <input type="hidden" name="id" value={{ $id }}>
                     <div class="row mt-3 p-0 px-0">
                         <div class="col-md-4 col-lg-4 mb-lg-0 mg-md-0 mb-2">
-                            <input type="text" name="title" class="form-control" placeholder="Drive title">
+                                <input type="text" name="title" class="form-control invalid" placeholder="Drive title"
+                                    id="validationServerUsername"
+                                    aria-describedby="inputGroupPrepend3 validationServerUsernameFeedback" required>
+                            </div>
+
+                        <div class="col-md-3 col-lg-4 mb-md-0 mb-2">
+                            <input type="date" name="date" id="date" class="form-control invalid" id="validationServerUsername"
+                            aria-describedby="inputGroupPrepend3 validationServerUsernameFeedback" required>
                         </div>
                         <div class="col-md-3 col-lg-4 mb-md-0 mb-2">
-                            <input type="date" name="date" id="date" class="form-control">
-                        </div>
-                        <div class="col-md-3 col-lg-4 mb-md-0 mb-2">
-                            <input type="text" name="conductedBy" class="form-control" placeholder="Conducted by">
+                            <input type="text" name="conductedBy" class="form-control invalid" placeholder="Conducted by" id="validationServerUsername"
+                            aria-describedby="inputGroupPrepend3 validationServerUsernameFeedback" required>
                         </div>
                     </div>
                     <div class="row mt-lg-3">
                         <div class="col-md-3 col-lg-4 mb-md-0 mb-2">
-                            <select name="driveType" id="" class="form-select">
+                            <select name="driveType" class="form-select invalid" id="validationServerUsername"
+                            aria-describedby="inputGroupPrepend3 validationServerUsernameFeedback" required>
                                 <option value="not selected" selected>Select drive type from list</option>
                                 <option value="regular">Regular NSS Activities</option>
                                 <option value="cleanliness">Cleanliness Drive</option>
                             </select>
                         </div>
                         <div class="col-md-3 col-lg-4 mb-md-0 mb-2">
-                            <input type="text" name="area" class="form-control" placeholder="Area">
+                            <input type="text" name="area" class="form-control invalid" placeholder="Area" id="validationServerUsername"
+                            aria-describedby="inputGroupPrepend3 validationServerUsernameFeedback" required>
                         </div>
                         <div class="col-md-4 col-lg-4 mb-2">
-                            <input type="time" name="from" class="form-control" placeholder="From">
+                            <input type="time" name="from" class="form-control invalid" placeholder="From" id="validationServerUsername"
+                            aria-describedby="inputGroupPrepend3 validationServerUsernameFeedback" required>
                         </div>
                     </div>
                     <div class="row mt-lg-3">
                         <div class="col-md-4 col-lg-4 mb-2">
-                            <input type="time" name="to" class="form-control" placeholder="To">
+                            <input type="time" name="to" class="form-control invalid" placeholder="To" id="validationServerUsername"
+                            aria-describedby="inputGroupPrepend3 validationServerUsernameFeedback" required>
                         </div>
                         <div class="col mb-2">
-                            <textarea name="description" id="" cols="30" rows="6" class="form-control description"
-                                placeholder="Add description about the drive"></textarea>
+                            <textarea name="description" id="" cols="30" rows="6" class="form-control description invalid"
+                                placeholder="Add description about the drive" id="validationServerUsername"
+                                aria-describedby="inputGroupPrepend3 validationServerUsernameFeedback" required></textarea>
                         </div>
                     </div>
                     <div class="row mt-lg-3">
